e1929b26b16274f7d108e455ab92bbe587597f21
kbarne12 <kbarne12@gmail.com>
2021-03-18 23:37:56 -0400

    db connection

---
+++ express-mongoose-flights/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/flights', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}
+);
+
+const db = mongoose.connection
+
+db.on('connected', function() {
+    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`)
+  })
